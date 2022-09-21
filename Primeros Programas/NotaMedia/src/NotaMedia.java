42b15868feda17b8a5ef758d47eee2bb5ce6d36f
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2022-09-21 14:00:48 +0200

    Update NotaMedia.java

--- Primeros Programas/NotaMedia/src/NotaMedia.java
+++ Primeros Programas/NotaMedia/src/NotaMedia.java
@@ -1,6 +1,21 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * ENUNCIADO DEL EJERCICIO:
+ 
+Escribe un programa que pregunte el nombre al alumno.
+
+Después le preguntará la nota (que puede tener decimales) de los 6 módulos que está cursando:
+
+"Qué nota tienes en el módulo 1?"
+
+"Qué nota tienes en el módulo 2?"
+
+..................
+
+"Qué nota tienes en el módulo 6?"
+
+Al finalizar le devolverá un mensaje en el que le indicará la nota media del curso (redondeando al entero inferior).
+
+"Hola xxxxxx tu nota media en el curso es: "
  */
 package notamedia;
 

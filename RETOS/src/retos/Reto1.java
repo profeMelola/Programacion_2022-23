a3021ca3a7f3ce36d18ea598d66abb2b6c4c6d54
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2022-09-30 12:36:44 +0200

    Update Reto1.java

--- RETOS/src/retos/Reto1.java
+++ RETOS/src/retos/Reto1.java
@@ -22,19 +22,22 @@ public class Reto1 {
 
         System.out.print("Introduzca un número entre 0 y 99.999: ");
         int num = sc.nextInt();
-
-        if (num < 10) {
-            System.out.println("Tiene 1 cifra");
-        } else if (num < 100) {
-            System.out.println("Tiene 2 cifras");
-        } else if (num < 1000) {
-            System.out.println("Tiene 3 cifras");
-        } else if (num < 10000) {
-            System.out.println("Tiene 4 cifras");
-        } else if (num < 100000) {
-            System.out.println("Tiene 5 cifras");
-        }
-
+        
+        if (num >= 0 && num <=99999){
+        
+            if (num < 10) {
+                System.out.println("Tiene 1 cifra");
+            } else if (num < 100) {
+                System.out.println("Tiene 2 cifras");
+            } else if (num < 1000) {
+                System.out.println("Tiene 3 cifras");
+            } else if (num < 10000) {
+                System.out.println("Tiene 4 cifras");
+            } else if (num < 100000) {
+                System.out.println("Tiene 5 cifras");
+            }
+        }else
+            System.out.println("Has introducido un número incorrecto");
     }
 
 }

663e1359298cfb7119a39dd9daab0a8c0fefc4f9
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2022-10-15 14:00:44 +0200

    Update Main.java

--- UT03_Estructuras_Control/FigurasGeometricas/src/es/daw/figurasgeometricas/Main.java
+++ UT03_Estructuras_Control/FigurasGeometricas/src/es/daw/figurasgeometricas/Main.java
@@ -7,57 +7,58 @@ import java.util.Scanner;
 
 /**
  *
- * @author melol
+ * @author melola
  */
-public class Main {
+public class FigurasGeometricas {
 
-    //Declaración del scanner a nivel global de la clase
     static Scanner sc;
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-        // Inicialización del scanner
+        
         sc = new Scanner(System.in);
+        
+        String paBorrar= "hola";
+        
         int opcion = 0;
-
+        
         do{
+            
             opcion = elegirOpcionMenu();
-            switch (opcion) {
+            
+            switch (opcion){
                 case 1:
+                    // PINTAR RECTÁNGULO
                     System.out.println("Indica la base y la altura");
                     int base = sc.nextInt();
                     int altura = sc.nextInt();
-                    pintaRectangulo(base, altura);
+                    pintaRectangulo(base,altura);
+                    
                     break;
                 case 2:
-                    System.out.println("Indica el valor entero del lado del triángulo rectángulo:");
-                    int lado = sc.nextInt();
-                    pintaTriangulo(lado);
+                    System.out.println("Indica el lado: ");
+                    pintaTriangulo(sc.nextInt());
                     break;
                 case 3:
-                    System.out.println("Indica el número de filas de la pirámide");
-                    int filas = sc.nextInt();
-                    pintaPiramide(filas);
-                    break;
-                case 0:
-                    System.out.println("El programa se va a cerrar!");
+                    //pendiente
                     break;
                 default:
                     System.out.println("Elige una opción válida!");
+                
             }
-        }while(opcion != 0);
-
+            
+            
+        }while (opcion != 0);
+        
     }
-
+    
     /**
-     * Método que pinta el menú por consola
-     *
-     * @return devuelve la opción del menú
+     * Método estático (o función) que muestra el menú y devuelve
+     * la opción elegida por el usuario
+     * @return 
      */
-    public static int elegirOpcionMenu() {
+    public static int elegirOpcionMenu(){
         System.out.println("*********************************************");
         System.out.println("********** FIGURAS GEOMÉTRICAS **************");
         System.out.println("*********************************************");
@@ -68,99 +69,40 @@ public class Main {
         System.out.println("-------------------");
         System.out.print("Elige una opción: ");
 
-        if (sc.hasNextInt()) {
+        if(sc.hasNextInt())
             return sc.nextInt();
-        } else {
-            return 4; //devuelve una opción no válida cualquiera
-        }
+        else
+            return 4; // devuelvo una opción no válida cualquiera
+        
     }
-
+    
     /**
-     * Método que pinta un rectángulo
-     *
+     * Método estático que pinta con asteriscos un rectángulo por consola
      * @param base
-     * @param altura
+     * @param altura 
      */
-    public static void pintaRectangulo(int base, int altura) {
-        System.out.println("RECTÁNGULO de base " + base + " y altura " + altura);
-        //System.out.println("Solución 1:");
-        /*
-        for (int i = 0; i < altura; i++) {
-            String fila = "";
-
-            for (int j = 0; j < base; j++) {
-                fila += "* ";
-            }
-            System.out.println(fila);
-        }
-
-        System.out.println("Solución 2:");*/
-        for (int i = 0; i < altura; i++) {
-            for (int j = 0; j < base; j++) {
+    public static void pintaRectangulo(int base, int altura){
+        
+        for (int i=1; i <= altura; i++){
+            for (int j=1; j <= base; j++)
                 System.out.print("* ");
-            }
             System.out.println(" ");
         }
+        
     }
-
+    
     /**
-     * Método que pinta un triangulo rectángulo de dos formas
+     * pintaTriangulo
      * @param lado 
      */
-    public static void pintaTriangulo(int lado) {
-        System.out.println("TRIÁNGULO de lado " + lado + " : primera forma");
-        for (int x = 1; x <= lado; x++) {
-            for (int i = 1; i <= x; i++) {
+    public static void pintaTriangulo(int lado){
+        
+        for (int i=1; i<= lado; i++){
+            for(int j=1; j<= i; j++){
                 System.out.print("* ");
             }
-
-            System.out.println("");
-        }
-
-        System.out.println("TRIÁNGULO de lado " + lado + " : segunda forma");
-        for (int x = 1; x <= lado; x++) {
-            for (int i = x; i <= lado; i++) {
-                System.out.print("* ");
-            }
-
             System.out.println("");
         }
     }
     
-    /**
-     * Método que pinta una pirámide de n filas de dos formas
-     * @param numFilas 
-     */
-    public static void pintaPiramide(int numFilas){
-        String blancos;
-        System.out.println("PIRÁMIDE NORMAL");
-        for ( int i=numFilas; i>=1; i--){
-            // TRIÁNGULO NORMAL
-            blancos = "";
-            for(int j=1; j<i; j++){
-                blancos +=" ";
-            }
-            
-            System.out.print(blancos);
-            for ( int k = (i*2)-1; k < (numFilas*2); k++)
-                System.out.print("*");
-            System.out.println(blancos);
-        }        
-        
-        System.out.println("\nPIRÁMIDE INVERTIDA:");
-        for ( int i=numFilas; i>=0; i--){
-            // TRIÁNGULO INVERTIDO
-            //calcular blancos
-            blancos = "";
-            for (int j=0; j<(numFilas-i); j++)
-                blancos += " ";
-            
-            System.out.print(blancos);
-            for ( int k = 0; k < (i*2)-1; k++)
-                System.out.print("*");
-            System.out.println(blancos);
-        }
-        
-        
-    }
 }

e134fc0c9de88897fca1457c0d2289d6ead7ecc7
Mari Luz Elola Rubio <91023374+profeMelola@users.noreply.github.com>
2022-12-01 11:17:01 +0100

    Add files via upload

--- UT05_Introducción POO/TarjetaCredito/src/es/daw/poo/TarjetasCreditoApp.java
+++ UT05_Introducción POO/TarjetaCredito/src/es/daw/poo/TarjetasCreditoApp.java
@@ -3,6 +3,8 @@
  */
 package es.daw.poo;
 
+import es.daw.poo.model.Tarjeta;
+
 /**
  *
  * @author melola
@@ -13,7 +15,44 @@ public class TarjetasCreditoApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //Provocar fallo: al comprar no tengo saldo
+        Tarjeta t1 = new Tarjeta("666", "Mari", 500);
+        
+        Tarjeta t2 = new Tarjeta("11111","Paula",1000);
+        
+        //Provocar fallo de compra por límite
+        Tarjeta t3 = new Tarjeta("222222","Iván G",5000,10);
+
+        //Da error estos dos casos
+        t1.pagar(550);
+        
+        t3.pagar(11);
+
+        
+        // SIMULAMOS QUE SE DAN DE ALTA 3 TARJETAS
+        // ESTO SE HA PEDIDO POR SCANNER
+        Tarjeta[] tarjetas = new Tarjeta[3];
+        tarjetas[0] = t1;
+        tarjetas[1] = t2;
+        tarjetas[2] = t3;
+        
+        imprimeTarjetas(tarjetas);
+        
+        //compra correcta de 250 euros
+        t2.pagar(250);
+        
+        imprimeTarjetas(tarjetas);
+        
+        
+    }
+    
+    public static void imprimeTarjetas(Tarjeta[] tarjetas){
+        // INFORME DE TARJETAS DADAS DE ALTA EN EL SISTEMA
+        System.out.println("* INFORME DE TARJETAS DADAS DE ALTA EN EL SISTEMA");
+        for(Tarjeta t: tarjetas)
+            System.out.println(t);
+        
     }
     
 }
--- UT05_Introducción POO/TarjetaCredito/src/es/daw/poo/model/Tarjeta.java
+++ UT05_Introducción POO/TarjetaCredito/src/es/daw/poo/model/Tarjeta.java
@@ -1,6 +1,7 @@
 package es.daw.poo.model;
 
 import java.time.LocalDate;
+import java.util.Random;
 
 /**
  * TARJETA PREPAGO
@@ -21,18 +22,31 @@ public class Tarjeta {
     private int CVV;  //generar aleatoriamente un CVV desde 100 a 999
     private boolean activa; //en el momento de dar de alta la tarjeta queda activada
     private float saldo;
-    private float limite;
+    private float limite; // limite diario
     
     
     
     // CONSTRUCTORES
     public Tarjeta(String numero, String titular, float saldo) {
+        
         /*
+        // ------------------------------------
+        // Primera forma
         this.numero = numero;
         this.titular = titular;
         this.saldo = saldo;
         this.limite = LIMITE_DEFECTO;
+        
+        // - generar aleatoriamente un CVV desde 100 a 999
+        this.CVV = generarCVV();
+        // - activar la tarjeta
+        this.activa = true;
+        // - incrementar el contador de tarjetas emitidas
+        contador++;
+        // ----------------------------------------
         */
+        
+        // Segunda forma
         this(numero,titular,saldo,LIMITE_DEFECTO);
     }
 
@@ -42,7 +56,127 @@ public class Tarjeta {
         this.saldo = saldo;
         this.limite = limite;
         
-        //
+        // Pendiente realizar las operaciones automáticas al dar de alta
+        // una tarjeta en el sistema:
+        // - generar aleatoriamente un CVV desde 100 a 999
+        this.CVV = generarCVV();
+        // - activar la tarjeta
+        this.activa = true;
+        // - incrementar el contador de tarjetas emitidas
+        contador++;
+        // - automáticamente la fecha de caducidad será 1 año después de la fecha de alta
+        this.fechaCaducidad = generarFechaCaducidad();
+    }
+    
+    // GETTERS AND SETTERS
+
+    public static String getENTIDAD() {
+        return ENTIDAD;
+    }
+
+    public static float getLIMITE_DEFECTO() {
+        return LIMITE_DEFECTO;
+    }
+
+    public static int getContador() {
+        return contador;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public LocalDate getFechaCaducidad() {
+        return fechaCaducidad;
+    }
+
+    public int getCVV() {
+        return CVV;
+    }
+
+    public boolean isActiva() {
+        return activa;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public float getLimite() {
+        return limite;
+    }
+    
+    // setters..
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setLimite(float limite) {
+        this.limite = limite;
+    }
+
+    public void setActiva(boolean activa) {
+        this.activa = activa;
+    }
+    
+    
+    
+    // MÉTODOS PROPIOS
+    
+    /**
+     * Método para realizar el pago con la tarjeta
+     * @param importe
+     * @return 
+     */
+    public boolean pagar(float importe){
+        
+        boolean error;
+        
+        if (importe > saldo || importe > limite){
+            System.out.println("[ERROR] No se ha podido realizar el pago");
+            error = true;
+        }else{
+            saldo -= importe;
+            error = false;
+        }
+        
+        return error;
+        
+    }
+    
+    /**
+     * Método que genera un número aleatorio entre 100 y 999
+     * @return 
+     */
+    private int generarCVV(){
+        Random r = new Random();
+        //limiteInferior + r.nextInt((limiteSuperior +1) - limiteInferior)
+                
+        return 100+r.nextInt(900);
+    }
+    
+    /**
+     * Método que suma un año a la fecha actual
+     * @return 
+     */
+    private LocalDate generarFechaCaducidad(){
+        return LocalDate.now().plusYears(1);
+    }
+
+    @Override
+    public String toString() {
+        return "Tarjeta{" + "numero=" + numero + ", titular=" + titular + ", fechaCaducidad=" 
+                + fechaCaducidad + ", activa=" + activa + ", saldo=" + saldo 
+                + ", limite=" + limite + '}';
     }
     
     
